1655381afa04ac8ba9d36583538544155cf8ca07
VeerpalBrar <v.brar@mail.utoronto.ca>
2016-11-25 14:57:08 -0500

    Finish markov chain for order of 1.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # MarkovChain
 A simple markov chain created with python. Currently generates random text based off of Alice in Wonderland.
 
+#To Do:
+- make markov chain work for higher orders
+- sanitize text to fix contrations
+- all transition matrix to be saved to file and parsed from file to avoid generating it over and over again
+
 #Dependencies
 - textblob 0.11.1
 - numpy 1.11.2
--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@ from textblob import TextBlob
 import numpy
 
 
-with open('alice.txt', 'r') as content_file:
+with open(r'C:\Users\Veerpal\Documents\GitHub\markov\alice.txt', 'r') as content_file:
     content = content_file.read().replace('\n', '')
 
 print(type(content))
@@ -26,5 +26,23 @@ for i in range(1, len(allWords)):
 #divide element by row sum
 with numpy.errstate(divide='ignore',invalid='ignore'):
     transition = transition/transition.sum(axis=1, keepdims=True)
+    transition = numpy.cumsum(transition, axis =1)
+
+#begin text generation
+markov = []
+current = numpy.random.randint(0, len(uniqueWords))
+markov.append(uniqueWords[current])
+
+for i in range(50):
+    rand = numpy.random.random(1)[0]
+    index =0
+    while(transition[current][index] < rand):
+        index +=1
+    current = index
+    markov.append(uniqueWords[current])
+print("markov: ", " ".join(markov))
+
+
+
 
 
